954366e487f9d05a7cc62e18a20d20d0065bedab
rootwang1 <105794573+rootwang1@users.noreply.github.com>
2023-07-13 16:21:38 +0800

    Add files via upload

---
+++ python基础复习.py
@@ -0,0 +1,19 @@
+# a = 'wangsheng'
+# a_1 = """
+# woangsengsh112313213
+# """
+# b = len(a_1)
+# print(b)
+# print(a[0])
+# s = a.count('g')
+# print(s)
+# 条件控制语句
+if 1 > 10:
+    print('周子为')
+else:
+    print('泰拉瑞亚')
+# 循环语句
+for i in range(0, 100):
+    print(i)
+for i in range(0, 100, 10):
+    print(i)
