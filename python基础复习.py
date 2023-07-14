1eed6e3ea18e88c3897308c14c5c303bb3c90235
rootwang1 <105794573+rootwang1@users.noreply.github.com>
2023-07-14 11:46:04 +0800

    Update python基础复习.py

--- python基础复习.py
+++ python基础复习.py
@@ -17,21 +17,56 @@
 #     print(i)
 # for i in range(0, 100, 10):
 #     print(i)
+
+
 # python列表
-x = [1, 2, 3, 'abc']
-f = [1, 2, 3, 4]
-a = len(x)
-f.append(a)
-print(a)
-print(x[1])
-# 列表支持哪些操作
-# 两个列表相加
-s = x + f
-print(s)
-#删除
-x.remove(1)
-print(x)
-#数次数
-x.count(2)
-# 清空列表
-x.clear()
+# x = [1, 2, 3, 'abc']
+# f = [1, 2, 3, 4]
+# a = len(x)
+# f.append(a)
+# print(a)
+# print(x[1])
+# # 列表支持哪些操作
+# # 两个列表相加
+# s = x + f
+# print(s)
+# #删除
+# x.remove(1)
+# print(x)
+# #数次数
+# x.count(2)
+# # 清空列表
+# x.clear()
+
+
+# 元组
+# 元组不能更改
+# a = (1, 2, 3)
+# # print(a[0])
+# # 支持统计
+# # 检测下角标
+# s = a.index(3)
+# print(s)
+
+
+# 字典 dict
+# 格式 字典的键不能更改，通常使用数字和字符串，也可以是元组
+# 值可以是任何数据，数字，字符串，列表，元组等
+# a = {1: 2, 2: 3, 4: 6}
+# # 向字典里面添加数据
+# a[1] = ['wa', 'dsahi', 'das']
+# # print(a)
+# print(a.get(4))
+# # print()
+# # 输出字典都有什么
+# print(a.items())
+# # 输出多有key
+# print(a.keys())
+# # 输出所有values
+# print(a.values())
+# #清空字典
+# a.clear()
+# #删除键值对
+# del(a[1])
+# #弹出，使用健
+# a.pop(4)
