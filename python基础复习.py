03b2f5ff477ec120f483ff618cb7bafcd7cf4edf
rootwang1 <105794573+rootwang1@users.noreply.github.com>
2023-07-14 10:51:48 +0800

    Update python基础复习.py

--- python基础复习.py
+++ python基础复习.py
@@ -8,12 +8,30 @@
 # s = a.count('g')
 # print(s)
 # 条件控制语句
-if 1 > 10:
-    print('周子为')
-else:
-    print('泰拉瑞亚')
-# 循环语句
-for i in range(0, 100):
-    print(i)
-for i in range(0, 100, 10):
-    print(i)
+# if 1 > 10:
+#     print('周子为')
+# else:
+#     print('泰拉瑞亚')
+# # 循环语句
+# for i in range(0, 100):
+#     print(i)
+# for i in range(0, 100, 10):
+#     print(i)
+# python列表
+x = [1, 2, 3, 'abc']
+f = [1, 2, 3, 4]
+a = len(x)
+f.append(a)
+print(a)
+print(x[1])
+# 列表支持哪些操作
+# 两个列表相加
+s = x + f
+print(s)
+#删除
+x.remove(1)
+print(x)
+#数次数
+x.count(2)
+# 清空列表
+x.clear()
